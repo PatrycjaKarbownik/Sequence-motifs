9ec5ad16fa53c52f5c76cd363e717360dc08b216
Temebe <seboado@gmail.com>
2019-05-31 22:10:31 +0200

    Added neuronos

---
+++ src/FinalNeuron.py
@@ -0,0 +1,35 @@
+class FinalNeuron:
+    """Final neuron which has raw sequences
+
+    This neuron is different because we compare here not the logos based on probability but sequences."""
+    outputs = []
+
+    def __init__(self, max_error):
+        self.max_error = max_error
+
+    def append_output(self, sequence):
+        self.outputs.append(sequence)
+
+    def belongs(self, sequence):
+        for output in self.outputs:
+            error = 0
+            for nucleotide_a, nucleotide_b in zip(output, sequence):
+                if nucleotide_a != nucleotide_b:
+                    error += 1
+                if error > self.max_error:
+                    return False
+        return True
+
+
+if __name__ == "__main__":
+    sequence_a = "ATAGTA"
+    sequence_b = "ATACTA"
+    sequence_c = "ATAGCA"
+    sequence_d = "ATAGGA"
+    sequence_e = "ATGCCA"
+    final_neuron = FinalNeuron(2)
+    final_neuron.append_output(sequence_a)
+    final_neuron.append_output(sequence_b)
+    final_neuron.append_output(sequence_c)
+    print(final_neuron.belongs(sequence_d))
+    print(final_neuron.belongs(sequence_e))
---
+++ src/Neuron.py
@@ -0,0 +1,36 @@
+from src.Logo import Logo
+
+
+class Neuron:
+    outputs = []
+
+    def __init__(self, seq_size):
+        self.logo = Logo(seq_size)
+
+    def calculate_matching(self, sequence):
+        return self.logo.calculate_matching(sequence)
+
+    def append_sequence(self, sequence):
+        self.logo.load_sequence(sequence)
+
+    def append_output(self, neuron):
+        self.outputs.append(neuron)
+
+    def get_minimum_output(self, sequence):
+        """Calculating minimal matching value in output neurons
+
+        This function takes all of its outputs and calculates how similar the sequence is to this outputs.
+        We're searching for the least matching output category. Later network based on this minimal value will
+        assign new sequence to this neuron OR (if threshold value will be grater than minimal matching value) it will
+        create new neuron for this sequence.
+
+        """
+        min_value = 1
+        for output in self.outputs:
+            match = output.calculate_matching(sequence)
+            min_value = match if match < min_value else min_value
+        return min_value
+
+
+if __name__ == "__main__":
+    pass
