0a3d3b2049440caa4bf855e0a5d422187b81dc34
PatrycjaKarbownik <patrycja@karbownik.net>
2019-06-02 21:56:14 +0200

    printing complex motifs and sorting they by seq_amount

--- src/Motif.py
+++ src/Motif.py
@@ -4,12 +4,3 @@ class Motif:
         self.complex_motif = complex_motif
         self.motif = motif
         self.seq_amount = seq_amount
-
-    def get_seq_amount(self):
-        return self.seq_amount
-
-    def get_motif(self):
-        return self.motif
-
-    def get_complex_motif(self):
-        return self.complex_motif
