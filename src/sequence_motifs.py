7bd4c91c74869671c96acd5d41f28fa6fb307662
Nestus97 <K.B.Grabowski@stud.elka.pw.edu.pl>
2019-06-02 11:23:46 +0200

    Network from command line

--- src/sequence_motifs.py
+++ src/sequence_motifs.py
@@ -1,5 +1,8 @@
 import sys
 
+from src.Network import Network
+from src.input_patterns import load
+
 def printHelp():
     print("Wprowadziles zle argumenty. Sprawdz czy spelniaja wymagania:")
     print("- FILE_NAME           -> nazwa pliku w cudzyslowie, na przykład: \"data2.txt\"")
@@ -12,36 +15,71 @@ def printHelp():
 def checkValues(argList, count):
     if not isinstance(argList[1], str) or (argList[1].count(".txt") != 1):
         return False
-    argList[2] = int(argList[2])
     if argList[2] < 4:                             # or argList[2] < 4:
         return False
-    argList[3] = int(argList[3])
     if argList[3] < 2:
         return False
-    argList[4] = int(argList[4])
     if argList[4] < 0:
         return False
-    argList[5] = int(argList[5])
     if argList[5] < 2:
         return False
-    i = 6
-    while i < count:
-        argList[i] = float(argList[i])
-        if argList[i] < 0 or argList[i] > 1:
+    iterator = 6
+    while iterator < count:
+        if argList[iterator] < 0 or argList[iterator] > 1:
             return False
-        i = i+1
+        iterator = iterator+1
     return True
 
 if __name__ == "__main__":
 
     argList = sys.argv
-
-    count = 5+int(argList[3])
-    if len(argList) != count:
+    if len(argList) < 4 or len(argList) != 5+int(argList[3]):
         printHelp()
     else:
+        count = 5 + int(argList[3])
+        i = 2
+        while i < 6:
+            argList[i] = int(argList[i])
+            i = i+1
+        while i < count:
+            argList[i] = float(argList[i])
+            i = i+1
         if not checkValues(argList, count):
             print("Ilosc parametrow dobra, ale zle wartosci...")
             printHelp()
         else:
             print("gites")
+            TreshholdValues = []
+            i = 6
+            while i < count:
+                TreshholdValues.append(argList[i])
+                i = i+1
+            network = Network(argList[2], argList[3], argList[4], TreshholdValues, argList[5])
+            patterns = load(argList[2], argList[1])
+
+            print(patterns)
+            for pattern in patterns:
+                network.input(pattern)
+
+            print("\nRESULT FOR INITIAL TRY")
+            print("Sequence".rjust(5) + " Amount")
+            for n in network.final_neurons:
+                print(n.logo.get_motif() + " " + str(n.seq_amount))
+
+            leftovers = network.reduce_final_outputs()
+
+            print("\nRESULT WITHOUT REDUNDANT OUTPUTS")
+            print("Sequence".rjust(5) + " Amount")
+            for n in network.final_neurons:
+                print(n.logo.get_motif() + " " + str(n.seq_amount))
+
+            random.shuffle(leftovers)
+            for pattern in leftovers:
+                network.input(pattern)
+
+            print("\nRESULT AFTER USING LEFTOVERS")
+            print("Sequence".rjust(5) + " Amount")
+            for n in network.final_neurons:
+                if n.seq_amount < argList[5]:
+                    continue
+                print(n.logo.get_motif() + " " + str(n.seq_amount))
