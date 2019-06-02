3c826e559440668aabd5bd6c30af9de7d8f142e4
PatrycjaKarbownik <patrycja@karbownik.net>
2019-06-02 22:06:09 +0200

    motif class described

--- src/Motif.py
+++ src/Motif.py
@@ -1,5 +1,11 @@
 class Motif:
+    """Class that stores motif's attributes
 
+    This class is used to store information about motif:
+    motif sequence, which is a sequence with the most likely nucleotides,
+    motif with all possible nucleotides (sections separated by brackets)
+    and number of sequences which are represented by motif.
+    """
     def __init__(self, complex_motif, motif, seq_amount):
         self.complex_motif = complex_motif
         self.motif = motif
